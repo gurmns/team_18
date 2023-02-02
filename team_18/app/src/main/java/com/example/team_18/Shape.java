5d343391474e59e1ceb85319b0cb87ae570b5ff6
dreambigzhang <alan.yushu.zhang@gmail.com>
2023-02-01 19:39:56 -0700

    commit after conflict fixed

--- team_18/app/src/main/java/com/example/team_18/Shape.java
+++ team_18/app/src/main/java/com/example/team_18/Shape.java
@@ -2,9 +2,5 @@ package com.example.team_18;
 
 abstract class Shape {
     private int x, y;
-<<<<<<< HEAD
-    String color = "blue";
-=======
     String color = "Red";
->>>>>>> ff951d467d3ad9a3120395a7f6a2e61cc51bf946
 }
